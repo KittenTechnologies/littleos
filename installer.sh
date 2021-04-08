61f9667b3af7bbef866bae87d9d7ea27254ae889
KittenTechnologies <82116528+KittenTechnologies@users.noreply.github.com>
2021-04-08 14:28:45 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 # You have to run this with sudo or as root, otherwise, it won't work.
 read -p "Specify the download directory: " dir
+chmod 711 $dir/usr/bin/littleos
+chmod 711 $dir/usr/lib/lit/littleos
+chmod 711 -r $dir/usr/lib/lit/prog/
+chmod 777 -r $dir/usr/lib/lit/games/
+chmod 755 -r $dir/usr/lib/lit/man
 cp -p $dir/usr/bin/* /usr/bin
 cp -p -r $dir/usr/lib/lit/ /usr/lib/
