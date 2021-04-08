de9d2ba62cd4e59308baa3c78d2091212a4b799e
KittenTechnologies <82116528+KittenTechnologies@users.noreply.github.com>
2021-04-08 14:34:39 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,8 +3,8 @@
 read -p "Specify the download directory: " dir
 chmod 711 $dir/usr/bin/littleos
 chmod 711 $dir/usr/lib/lit/littleos
-chmod -r 711 $dir/usr/lib/lit/prog/
-chmod -r 777 $dir/usr/lib/lit/games/
-chmod -r 755 $dir/usr/lib/lit/man
+chmod 711 $dir/usr/lib/lit/prog/*
+chmod 777 $dir/usr/lib/lit/games/*
+chmod 755 $dir/usr/lib/lit/man*
 cp -p $dir/usr/bin/* /usr/bin
 cp -p -r $dir/usr/lib/lit/ /usr/lib/
