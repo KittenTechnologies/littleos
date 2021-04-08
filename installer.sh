a88a7460cb673c7eb5e8032f44634935b1bc1890
KittenTechnologies <82116528+KittenTechnologies@users.noreply.github.com>
2021-04-08 14:11:50 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # You have to run this with sudo or as root, otherwise, it won't work.
 read -p "Specify the download directory: " dir
-cp $dir/usr/bin/* /usr/bin
-cp -r $dir/usr/lib/lit/ /usr/lib/
+cp -p $dir/usr/bin/* /usr/bin
+cp -p -r $dir/usr/lib/lit/ /usr/lib/
